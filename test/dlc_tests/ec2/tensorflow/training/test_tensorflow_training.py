bbfb98110580d2a8f62f0c0fd86feb12e7b20e88
Elizaaaaa <elizazhang087@gmail.com>
2020-07-22 21:07:44 +0000

    update to the latest test_tensorflow_training.py

--- test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
+++ test/dlc_tests/ec2/tensorflow/training/test_tensorflow_training.py
@@ -20,25 +20,38 @@ TF_ADDONS_CMD = os.path.join(CONTAINER_TESTS_PREFIX, "testTFAddons")
 TF_EC2_GPU_INSTANCE_TYPE = get_ec2_instance_type(default="p2.8xlarge", processor="gpu")
 TF_EC2_CPU_INSTANCE_TYPE = get_ec2_instance_type(default="c5.4xlarge", processor="cpu")
 
+@pytest.mark.integration('general_tensorflow')
 def test_tensorflow_standalone_gpu(tensorflow_training, gpu_only):
     test_script = TF1_STANDALONE_CMD if is_tf1(tensorflow_training) else TF2_STANDALONE_CMD
     execute_ec2_training_test_indocker(tensorflow_training, test_script)
+
 
 def test_tensorflow_standalone_cpu(tensorflow_training, cpu_only):
     test_script = TF1_STANDALONE_CMD if is_tf1(tensorflow_training) else TF2_STANDALONE_CMD
     execute_ec2_training_test_indocker(tensorflow_training, test_script)
 
-# def test_tensorflow_train_mnist_gpu(tensorflow_training, gpu_only):
-#     execute_ec2_training_test_indocker(tensorflow_training, TF_MNIST_CMD)
+
+@pytest.mark.model("mnist")
+def test_tensorflow_train_mnist_gpu(tensorflow_training, gpu_only):
+    if is_tf1(tensorflow_training):
+        execute_ec2_training_test_indocker(tensorflow_training, TF1_KERAS_CMD)
+    else:
+        execute_ec2_training_test_indocker(tensorflow_training, TF_MNIST_CMD)
+
 
 # TODO: Change this back TF_EC2_CPU_INSTANCE_TYPE. Currently this test times out on c4.8x, m4.16x and t2.2x,
 #       though passes on all three when run manually. For now we are pinning to c5.18 until we can resolve the issue.
 def test_tensorflow_train_mnist_cpu(tensorflow_training, cpu_only):
-    execute_ec2_training_test_indocker(tensorflow_training, TF_MNIST_CMD)
+    if is_tf1(tensorflow_training):
+        execute_ec2_training_test_indocker(tensorflow_training, TF1_KERAS_CMD)
+    else:
+        execute_ec2_training_test_indocker(tensorflow_training, TF_MNIST_CMD)
+
 
 def test_tensorflow_with_horovod_gpu(tensorflow_training, gpu_only):
     test_script = TF1_HVD_CMD if is_tf1(tensorflow_training) else TF2_HVD_CMD
-    execute_ec2_training_test_indocker(tensorflow_training, test_script)
+    execute_ec2_training_test_indocker(tensorflow_training, test_script, large_shm=True)
+
 
 # TODO: Change this back TF_EC2_CPU_INSTANCE_TYPE. Currently this test times out on c4.8x, m4.16x and t2.2x,
 #       though passes on all three when run manually. For now we are pinning to c5.18 until we can resolve the issue.
@@ -46,38 +59,63 @@ def test_tensorflow_with_horovod_cpu(tensorflow_training, cpu_only):
     test_script = TF1_HVD_CMD if is_tf1(tensorflow_training) else TF2_HVD_CMD
     execute_ec2_training_test_indocker(tensorflow_training, test_script)
 
+
 def test_tensorflow_opencv_gpu(tensorflow_training, gpu_only):
     if is_tf1(tensorflow_training):
         pytest.skip("This test is for TF2 only")
     execute_ec2_training_test_indocker(tensorflow_training, TF_OPENCV_CMD)
+
 
 def test_tensorflow_opencv_cpu(tensorflow_training, cpu_only):
     if is_tf1(tensorflow_training):
         pytest.skip("This test is for TF2 only")
     execute_ec2_training_test_indocker(tensorflow_training, TF_OPENCV_CMD)
 
+
 # Testing Telemetry Script on only one GPU instance
 def test_tensorflow_telemetry_gpu(tensorflow_training, gpu_only):
     execute_ec2_training_test_indocker(tensorflow_training, TF_TELEMETRY_CMD)
 
+
 # Testing Telemetry Script on only one CPU instance
 def test_tensorflow_telemetry_cpu(tensorflow_training, cpu_only):
     execute_ec2_training_test_indocker(tensorflow_training, TF_TELEMETRY_CMD)
+
 
 def test_tensorflow_keras_horovod_amp(tensorflow_training, gpu_only):
     if is_tf1(tensorflow_training) or is_tf20(tensorflow_training):
         pytest.skip("This test is for TF2.1 and later only") # https://github.com/tensorflow/tensorflow/issues/33484#issuecomment-555299647
     execute_ec2_training_test_indocker(tensorflow_training, TF_KERAS_HVD_CMD_AMP)
 
+
 def test_tensorflow_keras_horovod_fp32(tensorflow_training, gpu_only):
     if is_tf1(tensorflow_training):
         pytest.skip("This test is for TF2 and later only")
     execute_ec2_training_test_indocker(tensorflow_training, TF_KERAS_HVD_CMD_FP32)
 
+
 # Testing Tensorboard with profiling
 def test_tensorflow_tensorboard_gpu(tensorflow_training, gpu_only):
+    if is_tf1(tensorflow_training):
+        pytest.skip("This test is for TF2 only")
     execute_ec2_training_test_indocker(tensorflow_training, TF_TENSORBOARD_CMD)
+
 
 # Testing Tensorboard with profiling
 def test_tensorflow_tensorboard_cpu(tensorflow_training, cpu_only):
+    if is_tf1(tensorflow_training):
+        pytest.skip("This test is for TF2 only")
     execute_ec2_training_test_indocker(tensorflow_training, TF_TENSORBOARD_CMD)
+
+# TensorFlow Addons is actively working towards forward compatibility with TensorFlow 2.x
+# https://github.com/tensorflow/addons#python-op-compatility
+def test_tensorflow_addons_gpu(tensorflow_training, gpu_only):
+    if is_tf1(tensorflow_training):
+        pytest.skip("This test is for TF2 only")
+    execute_ec2_training_test_indocker(tensorflow_training, TF_ADDONS_CMD)
+
+
+def test_tensorflow_addons_cpu(tensorflow_training, cpu_only):
+    if is_tf1(tensorflow_training):
+        pytest.skip("This test is for TF2 only")
+    execute_ec2_training_test_indocker(tensorflow_training, TF_ADDONS_CMD)
